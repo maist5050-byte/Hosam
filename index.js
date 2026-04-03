4fabf2d7f4acbdcdf2a102975c880bd647a18b72
maist5050-byte <maist5050@gmail.com>
2026-04-03 17:38:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
-  const proData = {
-    "y4": 0.65,
-    "x4": 0.55,
-    "aim_lock": 0,
-    "headshot_rate": 75,
-    "recoil": 0.15,
-    "damage": 1.45
+  const ultraData = {
+    "y4": 1.45,           // سحب خارق للفوق عشان الهيد يجي بلمحة (كان 1.25)
+    "x4": 0.95,           // ثبات كامل لليمين واليسار
+    "aim_lock": 0,        // خليه 0 عشان الإيم ما يثبت في الصدر ويطلع للرأس بسلاسة
+    "headshot_rate": 85,  // طلبك: الهيدشوت صار 85%
+    "recoil": 0.02,       // الارتداد شبه مخفي تماماً
+    "damage": 1.70        // دمج قوي جداً
   };
-  res.end(JSON.stringify(proData));
+  res.end(JSON.stringify(ultraData));
 });
 server.listen(process.env.PORT || 3000);
